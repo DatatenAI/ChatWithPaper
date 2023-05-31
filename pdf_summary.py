0d1e927d98f148cb17a56e8933eb6a31e8084f18
SRHER <125043700+SRHER@users.noreply.github.com>
2023-05-31 13:01:33 +0800

    Update pdf_summary.py

--- pdf_summary.py
+++ pdf_summary.py
@@ -367,6 +367,8 @@ async def conclude_basic_information(path: str, text: str, lang: str):
         3. Indicate the first author's affiliation (with Chinese translation)
         4. Highlight the keywords of this article (in English)
         5. Provide links to the paper and GitHub code (if available; if not, use "GitHub: None")
+        6. You should first summarize this work in one sentence, the language should be rigorous, in the style of a popular science writer, including what problems, what methods were used, were solved and what results were achieved. (you should output as {lang}!)
+        7. Whole research background, output as {lang}!
         
 Organize your response using the following markdown structure:
 
@@ -378,17 +380,15 @@ Organize your response using the following markdown structure:
 - Keywords: xxx
 - URLs: xxx or xxx , xxx
 
-(Next, you should only output as {lang} language until the end! unless necessary English items)
 # Brief introduction :
 
-- xxx (You should first summarize this work in one sentence, the language should be rigorous, in the style of a popular science writer,
-including what problems, what methods were used, were solved and what results were achieved. you should output as {lang}!)
+- xxx 
 
 # Background:
 
-- BackGround: xxx (Whole research background, output as {lang}!).
+- BackGround: xxx
 - Past methods: xxx (Introduce past methods and their problems, output as {lang}!)        
-- Motivation: xxx (Motivation: How does the author move from background knowledge to the research in this paper, output as {lang}!)        
+- Motivation: xxx (Motivation: How does the author move from background knowledge to the research in this paper, output as {lang}!)
 
 Remember to:
 - Ensure that the response strictly follows the provided format and does not include any additional content. 
