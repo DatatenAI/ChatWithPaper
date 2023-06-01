80b1831729ed0573c4a00695c26608ff966ea639
Rongkang Xiong <earth@mail.ustc.edu.cn>
2023-06-01 22:38:30 +0800

    fix FREE_TOKEN

--- user_db.py
+++ user_db.py
@@ -1,7 +1,7 @@
 import redis_manager
 from constants import FREE_TOKEN
 import db
-
+from loguru import logger
 async def update_token_consumed_paid(user_id: str,
                                      token_consumed_increase: float):
     lock_name = "consumed_token_paid" + user_id
@@ -12,11 +12,16 @@ async def update_token_consumed_paid(user_id: str,
     try:
         result = db.UserToken.get_or_none(db.UserToken.user_id == user_id)
         if not result:
+            logger.error(f"User: {user_id}, not found")
             raise Exception("User not found")
         if result.tokens_purchased < result.tokens_consumed + token_consumed_increase:
+            logger.error(f"token paid consumption exceeded, user_id:{user_id}, "
+                         f"tokens_consumed:{result.tokens_consumed},"
+                         f"token_purchased:{result.tokens_purchased}")
             raise Exception("Token consumption exceeded")
         db.UserToken.update(tokens_consumed=db.UserToken.tokens_consumed + token_consumed_increase).where(
             db.UserToken.user_id == user_id).execute()
+        logger.info(f"user_id:{user_id}, take paid {token_consumed_increase} tokens")
     finally:
         await redis_manager.release_lock(lock_name, lock)
 
@@ -30,9 +35,14 @@ async def update_token_consumed_free(user_id: str,
     try:
         user = db.User.get_or_none(db.User.user_id == user_id)
         if not user:
+            logger.error(f"User: {user_id}, not found")
             raise Exception("User not found")
         if user.token_consumed + token_consumed_increase > FREE_TOKEN:
+            logger.error(
+                f"token free consumption exceeded, user_id:{user_id}, tokens_consumed:{user.token_consumed}, "
+                f"token_increase:{token_consumed_increase}")
             raise Exception("Token consumption exceeded")
         db.User.update(token_consumed=db.User.token_consumed + token_consumed_increase).where(db.User.user_id == user_id).execute()
+        logger.info(f"user_id:{user_id}, take free {token_consumed_increase} tokens")
     finally:
         await redis_manager.release_lock(lock_name, lock)
